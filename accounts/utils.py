bc7254bb96063b8f3ff274beabe07b9f07a74390
chris-y <maxsummer256@gmail.com>
2021-11-12 14:02:26 +0900

    add: SignUp, SignIn view

---
+++ accounts/utils.py
@@ -0,0 +1,28 @@
+import jwt
+
+from django.http            import JsonResponse
+
+from eightpercent.settings  import SECRET_KEY, ALGORITHM
+from models import User
+
+def login_decorator(func):
+    def wrapper(self, request, *args, **kwargs):
+        if 'Authorization' not in request.headers: 
+            return JsonResponse ({'message' : 'UNAUTHORIZED'}, status=401)
+
+        access_token = request.headers.get('Authorization')
+        
+        try:
+            payload      = jwt.decode(access_token, SECRET_KEY, algorithms=ALGORITHM)
+            user         = User.objects.get(id=payload['user_id'])
+            request.user = user
+
+        except jwt.exceptions.DecodeError:
+            return JsonResponse({'MESSAGE': 'INVALID_TOKEN'}, status=401)
+
+        except User.DoesNotExist:
+            return JsonResponse({'MESSAGE': 'INVALID_USER'}, status=401)
+
+        return func(self, request,  *args, **kwargs)
+
+    return wrapper
