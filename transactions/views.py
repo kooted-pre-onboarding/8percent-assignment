6bdb84eb8d13a2ef0dd43d2b242fedf67cdd75f0
김지훈 <kimvz2088@gmail.com>
2021-11-12 16:17:10 +0900

    MODIFY feature/deposit_withdraw

--- transactions/views.py
+++ transactions/views.py
@@ -6,19 +6,19 @@ from django.http    import JsonResponse
 from django.views.generic.base import TemplateView
 
 from accounts.models import Account
+from accounts.utils  import login_decorator
 
 from transactions.models import Transaction, TransactionType
 
 
 class DepositTransactionView(TemplateView):
+    @login_decorator
     def post(self, request):
         try:
             data = json.loads(request.body)
-            # user = request.user
-            account = Account.objects.get(user_id = 1)
-            type_id = TransactionType.objects.get(type_id = 1)
+            user = request.user
+            account = Account.objects.get(user_id = user.id)
 
-            print(type_id)
             if data["amount"] <= 0:
                 return JsonResponse({"message" : "INVALID_INPUT"}, status=400)
         
@@ -26,46 +26,45 @@ class DepositTransactionView(TemplateView):
             account.save()
 
             Transaction.objects.create(
-                type_id = type_id,
-                amount=int(data["amount"]),
-                account_id = account.id,
+                transaction_type_id       = TransactionType.Name.DEPOSIT.value,
+                amount                    = int(data["amount"]),
+                account_id                = account.id,
                 balance_after_transaction = account.balance,
-                sum_up = data["sum_up"])
+                sum_up                    = data["sum_up"])
     
             return JsonResponse(
                 {"message": "SUCCESS", "deposit_balance": account.balance}, status=201)
 
-        # except TypeError:
-        #     return JsonResponse({"message": "TYPE_ERROR"}, status=400)
+        except TypeError:
+            return JsonResponse({"message": "TYPE_ERROR"}, status=400)
 
         except KeyError:
             return JsonResponse({"message": "KEY_ERROR"}, status=400)
 
 
 class WithdrawTransactionlView(TemplateView):
+    @login_decorator
     def post(self, request):
         try:
             data = json.loads(request.body)
-            #user = request.user
-            account = Account.objects.filter(user_id = 1)
+            user = request.user
+            account = Account.objects.get(user_id = user.id)
 
             if data["amount"] <= 0:
                 return JsonResponse({"message": "INVALID_INPUT"}, status=400)
 
-            if data["amounts"] > account.amount:
+            if data["amount"] > account.balance:
                 return JsonResponse({"message": "WRONG_REQUEST"}, status=400)
-
-            account = Account.objects.select_related('number')
             
-            account.amount -= data["amounts"]
+            account.balance -= data["amount"]
             account.save()
 
             Transaction.objects.create(
-                type = data['type'],
-                amount = data["amount"],
-                account_id = account.id,
+                transaction_type_id       = TransactionType.Name.WITHDRAW.value,
+                amount                    = data["amount"],
+                account_id                = account.id,
                 balance_after_transaction = account.balance,
-                sum_up = data["sum_up"])
+                sum_up                    = data["sum_up"])
 
             return JsonResponse(
                 {"message": "SUCCESS", "deposit_balance": account.balance}, status=201)
