2e316f8b9b6bb779730e9e378be0c104048f866b
palza4dev <moonsj6940@gmail.com>
2021-11-12 15:20:32 +0900

    Modify : models.py 수정 - 거래내역 업데이트 필드삭제

--- transactions/models.py
+++ transactions/models.py
@@ -4,17 +4,23 @@ from core.models     import TimeStampModel
 from accounts.models import Account
 
 class TransactionType(TimeStampModel):
-    type = models.CharField(max_length=50)
+    
+    class Name(models.IntegerChoices):
+        DEPOSIT  = 1
+        WITHDRAW = 2
+    
+    name = models.CharField(max_length=50)
     
     class Meta:
         db_table = 'transaction_types'
 
-class Transaction(TimeStampModel):
+class Transaction(models.Model):
     amount                    = models.PositiveIntegerField()
     balance_after_transaction = models.PositiveIntegerField()
     sum_up                    = models.CharField(max_length=100)
     account                   = models.ForeignKey(Account, on_delete=models.CASCADE)
     transaction_type          = models.ForeignKey(TransactionType, on_delete=models.CASCADE)
+    created_at                = models.DateTimeField(auto_now_add=True)
     
     class Meta:
         db_table = 'transactions'
