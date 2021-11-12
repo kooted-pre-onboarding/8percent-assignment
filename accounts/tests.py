004358f9ddfd02f01990b4d79262cdf6705243f6
chris-y <maxsummer256@gmail.com>
2021-11-12 14:02:26 +0900

    add: SignUp, SignIn view

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,106 @@
-from django.test import TestCase
+import random, bcrypt, jwt
 
-# Create your tests here.
+from django.test import TestCase, Client
+
+from accounts.models import User, Account
+from eightpercent.settings import SECRET_KEY, ALGORITHM
+
+client = Client()
+
+class SignUpTest(TestCase):
+    def setUp(self):
+        User.objects.create(name='hana', email='goodLuck@toyou.com', password='123abc!@')
+
+    def tearDown(self):
+        User.objects.all().delete()
+
+    def test_signup_success(self):
+        data = {
+            'name'     : 'thor', 
+            'email'    : 'haveAniceDay@toyou.com', 
+            'password' : '123abc!@'
+        }
+        response = client.post('/users/signup', data=data, content_type='application/json')
+        
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json(), {'message' : 'SUCCESS'})
+
+    def test_signup_invalid_email(self):
+        data = {
+            'name'     : 'ruru', 
+            'email'    : 'goodLuck@toyou.com', 
+            'password' : '123abc!@'
+        }
+        response = client.post('/users/signup', data=data, content_type='application/json')
+        
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {'message' : 'EXIST_EMAIL'})
+
+    def test_signup_keyerror(self):
+        data = {
+            'name'    : 'goodLuck@toyou.com',
+            'password' : '123abc@'
+        }
+
+        response = client.post('/users/signin', data=data, content_type='application/json')
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {'message' : 'KEY_ERROR'})
+
+class SignInTest(TestCase):
+    def setUp(self):
+        user = User.objects.create(
+            name     = 'hana', 
+            email    = 'goodLuck@toyou.com', 
+            password = bcrypt.hashpw('123abc!@'.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+        )
+
+        global access_token
+        access_token = jwt.encode({'user_id' : user.id}, SECRET_KEY, ALGORITHM)
+
+    def tearDown(self):
+        User.objects.all().delete()
+
+    def test_signin_success(self):
+        data = {
+            'email'    : 'goodLuck@toyou.com',
+            'password' : '123abc!@'
+        }
+
+        response = client.post('/users/signin', data=data, content_type='application/json')
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json(), {'access_token' : access_token})
+
+    def test_signin_invalid_email(self):
+        data = {
+            'email'    : 'goodLuck111@toyou.com',
+            'password' : '123abc@!'
+        }
+
+        response = client.post('/users/signin', data=data, content_type='application/json')
+
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json(), {'message' : 'INVALID_USER'})
+
+    def test_signin_invalid_user(self):
+        data = {
+            'email'    : 'goodLuck@toyou.com',
+            'password' : '123abc@'
+        }
+
+        response = client.post('/users/signin', data=data, content_type='application/json')
+
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json(), {'message' : 'INVALID_PASSWORD'})
+
+    def test_signin_keyerror(self):
+        data = {
+            'name'    : 'goodLuck@toyou.com',
+            'password' : '123abc@'
+        }
+
+        response = client.post('/users/signin', data=data, content_type='application/json')
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {'message' : 'KEY_ERROR'})
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,78 @@
-from django.shortcuts import render
+import json, re, bcrypt, jwt, random
 
-# Create your views here.
+from django.http  import JsonResponse
+from django.views import View
+from django.db    import transaction
+
+from accounts.models        import User, Account
+from eightpercent.settings  import SECRET_KEY, ALGORITHM
+
+class SignUp(View):
+    @transaction.atomic
+    def post(self, request):
+        data          = json.loads(request.body)
+        REGX_EMAIL    = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+        REGX_PASSWORD = '^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z!@#$%^&*]{8,20}$'
+
+        try:
+            if User.objects.filter(email=data['email']).exists():
+                return JsonResponse({'message': 'EXIST_EMAIL'}, status=400)
+
+            if not re.match(REGX_EMAIL, data['email']):
+                return JsonResponse({'message': 'INVALID_EMAIL_FORM'}, status=400)
+
+            if not re.match(REGX_PASSWORD, data['password']):
+                return JsonResponse({'message': 'INVALID_PASSWORD_FORM'}, status=400)
+
+            password = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+
+            # 기존 생성된 계좌번호들과 중복 방지를 위한 대조 / 유일한 신규계좌 발급
+            def Account_number():
+                def numbers():
+                    number = random.randint(1000, 9999)
+                    return number
+
+                account_number = f'{numbers()}-{numbers()}-{numbers()}-{numbers()}'
+                
+                if not Account.objects.filter(number=account_number).exists():
+                   return account_number
+
+                return Account_number()
+            
+            create_user = User(
+                name     = data['name'],
+                email    = data['email'],
+                password = password,
+            )
+
+            create_user.save()
+
+            Account(
+                number  = Account_number(),
+                user_id = create_user.id
+            ).save()
+
+            return JsonResponse({'message': 'SUCCESS'}, status=201)
+        
+        except KeyError:
+            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
+
+class SignIn(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+
+            if not User.objects.filter(email=data['email']).exists():
+                return JsonResponse({'message' : 'INVALID_USER'}, status=401)
+
+            user = User.objects.get(email=data['email'])
+
+            if not (user and bcrypt.checkpw(data['password'].encode('utf-8'), user.password.encode('utf-8'))):
+                return JsonResponse({'message' : 'INVALID_PASSWORD'}, status=401)
+
+        except KeyError:
+            return JsonResponse({'message' : 'KEY_ERROR'}, status=400)
+
+        access_token = jwt.encode({'user_id' : user.id}, SECRET_KEY, ALGORITHM)
+
+        return JsonResponse({'access_token' : access_token}, status=201)
